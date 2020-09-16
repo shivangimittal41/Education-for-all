a8ae97375ea77e245cdb7b64d919e3a261392330
Shivangi <shivangimittal4196@gmail.com>
2020-09-16 20:09:59 +0530

    Project

---
+++ Parivartan/Parivartan/MSForms.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Parivartan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MSForms : ContentPage
+    {
+        public MSForms()
+        {
+            InitializeComponent();
+        }
+    }
+}
